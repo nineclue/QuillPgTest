f28a988d5f62ae4ef5150bf67e4a61b1fad0a245
Suhku Huh <nineclue@gmail.com>
2019-12-02 09:12:48 +0900

    added macro implementation of SelectOrInsert macro

--- demo/src/DBFlexible.scala
+++ demo/src/DBFlexible.scala
@@ -1,18 +1,13 @@
-trait DBFlexible {
-    import io.getquill.context.Context
-    import DB.ctx._
+import io.getquill.context.{Context => dbContext}
+import scala.reflect.macros.whitebox.{Context => mContext}
+// import scala.reflect.macros.{Context => mContext}
+import scala.language.experimental.macros
+import scala.reflect.runtime.{universe => ru}
 
+trait DBFlexible {
     val id: Int
     val name: String
-    def selectOrInsert[A <: DBFlexible](tname: String, name: String)(implicit maker: String => A, encoder: Encoder[A]): Int = {
-        val dySchema = DB.ctx.dynamicQuerySchema[A](tname)
-        DB.ctx.run(dySchema.filter(_.name == lift(name)).map(_.id)) match {
-            case Nil => 
-                DB.ctx.run(dySchema.insertValue(quote(lift(maker(name)))).returningGenerated(_.id))
-            case iid::_ => 
-                iid
-        }
-    }
+    def selectOrInsert[A <: DBFlexible](name: String, maker: String => A): Int = macro DBFlexible.selectOrInsertImpl[A]
     /*
     val dynamicSchema = context.dynamicQuerySchema[MyDBClass](tableNameVar)
 
@@ -22,4 +17,20 @@ trait DBFlexible {
         }
     }
     */
+}
+
+object DBFlexible {
+    def selectOrInsertImpl[A: c.WeakTypeTag](c: mContext)(name: c.Tree, maker: c.Tree): c.Tree = {
+        import c.universe._
+
+        q"""
+        import DB.ctx._
+        DB.ctx.run(dySchema.filter(_.name == lift(name)).map(_.id)) match {
+            case Nil => 
+                DB.ctx.run(dySchema.insertValue(quote(lift(maker(name)))).returningGenerated(_.id))
+            case iid::_ => 
+                iid
+        }
+        """
+    }
 }
